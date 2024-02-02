e10134af7ea14eacc2580187ecbf72aecf5d9eda
Chahat Anthony <chahatanthony@gmail.com>
2024-02-02 08:26:47 +0100

    up

--- Looter/LootDetailsView.swift
+++ Looter/LootDetailsView.swift
@@ -29,7 +29,7 @@ struct LootDetailsView: View {
                 VStack(alignment: .leading) {
                     HStack {
                         if let cover = item.game.coverName {
-                            
+                            Image(cover)
                         } else {
                             VStack {
                                 Image(systemName: "rectangle.slash").padding(.horizontal, 8)
